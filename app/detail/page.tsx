9d3c0541026920be38de923c3e15aeee32c4f2d6
hujeong <hujeong@student.42seoul.kr>
2023-07-06 15:42:43 +0900

    feat: main, detail, write 페이지 생성

---
+++ app/detail/page.tsx
@@ -0,0 +1,9 @@
+const detailPage = () => {
+  return (
+    <div>
+      <h2>detail</h2>
+    </div>
+  )
+}
+
+export default detailPage;
---
+++ app/write/page.tsx
@@ -0,0 +1,9 @@
+const detailPage = () => {
+  return (
+    <div>
+      <h2>write</h2>
+    </div>
+  )
+}
+
+export default detailPage;
