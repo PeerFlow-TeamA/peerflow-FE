428e43de75aa0d556b3c24c4c37d30c5fafaf00f
hujeong <hujeong@student.42seoul.kr>
2023-07-10 17:23:36 +0900

    design: 질문목록 배치

--- app/page.tsx
+++ app/page.tsx
@@ -1,80 +1,111 @@
 'use client'
-import { Container, Pagination, Stack, Grid } from '@mui/material';
-// import GnbContext from '../hooks/GnbContext';
+import { Container, Pagination, Stack, Grid, Divider, TextField, Box, InputAdornment, IconButton } from '@mui/material';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import QuestionListItem from '../components/QuestionListItem';
 import React, { Fragment } from 'react';
+import './globals.css';
 
-import Button from "@mui/material/Button";
 
-// function ContainButton() {
-//   return <Button variant="contained" size="small" >Hello World</Button>;
-// }
+type item = {
+  index: number;
+  title: string;
+}
 
-export default function Home() {
+function Header() {
   return (
-    <>
-      <Container sx={{ display: 'flex', gap: 1, flexDirection: 'column' }}>
-        <QuestionListItem index={1} title={"hi"} />
-        <QuestionListItem index={2} title={"hi"} />
-        <QuestionListItem index={3} title={'hi'} />
-      </Container>
-      <Grid container spacing={1}>
-        <Grid item xs={12} sm={6}>
-          <Button color='secondary' variant='text' size='small' disableElevation={true} disableRipple={true} >Hello world </Button>
-        </Grid>
-      </Grid >
-      <Stack>
-        <Pagination count={5} showFirstButton showLastButton />
-      </Stack>
-    </>
+    <header>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          bgcolor: 'background.paper',
+          borderRadius: 1,
+        }}>
+        <h2>Peer flow</h2>
+      </Box>
+    </header>
+  )
+}
+
+function SearchBox() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row-reverse',
+        bgcolor: 'background.paper',
+        borderRadius: 1,
+      }}
+    >
+      <TextField
+        id="outlined-search"
+        type="search"
+        placeholder="검색"
+        size={'small'}
+        InputProps={{
+          endAdornment: (
+            < InputAdornment position={'end'}>
+              <IconButton
+                onClick={() => {
+                }}
+              >
+                <SearchOutlinedIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+    </Box>
+  )
+}
+
+function QuestionPagination() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        p: 1,
+        m: 1,
+        bgcolor: 'background.paper',
+        borderRadius: 1,
+      }}
+    >
+      <Pagination count={5} showFirstButton showLastButton />
+    </Box>
   );
 }
 
+export default function Home() {
+  const items: item[] = [
+    {
+      index: 1,
+      title: "minishell",
+    },
+    {
+      index: 2,
+      title: "ft_irc",
+    },
+    {
+      index: 3,
+      title: "minirt",
+    }
+  ];
 
-  //   <Container sx={{ display: 'flex', gap: 1, flexDirection: 'column' }}>
-  //     <Stack justifyContent={'space-between'} direction="row" mt={1}>
-  //       <TextField
-  //         id="outlined-search"
-  //         type="search"
-  //         placeholder="제목 검색"
-  //         size={'small'}
-  //         onChange={(e) => {
-  //           setTitle(e.target.value)
-  //         }}
-  //         value={title}
-  //         InputProps={{
-  //           endAdornment: (
-  //             <InputAdornment position={'end'}>
-  //               <IconButton
-  //                 onClick={() => {
-  //                   setCategory('search')
-  //                   setSearch(true)
-  //                 }}
-  //               >
-  //                 <SearchOutlinedIcon />
-  //               </IconButton>
-  //             </InputAdornment>
-  //           ),
-  //         }}
-  //       />
-  //       <DefaultDropdown setSort={setSort} sort={sort} />
-  //     </Stack>
-  //     {data?.content?.length ? (
-  //       data?.content.map((item: Post) => {
-  //         return <DefaultCard data={item} key={item.questionId} />
-  //       })
-  //     ) : (
-  //       <Stack direction={'row'} justifyContent={'center'} width={'100%'}>
-  //         <Box sx={{ marginTop: '4px' }}>게시글이 없습니다</Box>
-  //       </Stack>
-  //     )}
-  //     <Stack justifyContent={'center'} direction="row" m={1}>
-  //       <DefaultPagination
-  //         count={data?.totalPages}
-  //         page={page}
-  //         setPage={setPage}
-  //         category={category}
-  //       />
-  //     </Stack>
-  //   </Container>
-  // )
+  return (
+    <Container fixed>
+      <Header />
+      <Stack
+        spacing={2}
+        divider={<Divider orientation="horizontal" flexItem />}>
+        <SearchBox />
+        {items.map((item) => {
+          return (
+            <QuestionListItem key={item.index} index={item.index} title={item.title} />
+          );
+        })}
+        <QuestionPagination />
+      </Stack>
+    </Container>
+  );
+}
--- components/QuestionListItem.tsx
+++ components/QuestionListItem.tsx
@@ -1,13 +1,16 @@
-import { Fragment } from "react";
+
 import React from 'react';
-import { Stack } from "@mui/material";
+import { Stack, Link } from "@mui/material";
 
 function QuestionListItem({ index, title }: { index: number, title: string }): JSX.Element {
   return (
-    <Stack spacing={2} direction="row" mt={1}>
-      <div>{index}</div>
-      <div>{title}</div>
-    </Stack>
+    <Link href='/detail' underline="none" color="none">
+      <Stack spacing={3} direction="row" mt={3}>
+        <div>{index}</div>
+        <div>{title}</div>
+      </Stack>
+    </Link>
+
   );
 }
 
