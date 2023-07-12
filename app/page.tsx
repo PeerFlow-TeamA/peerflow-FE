a0d5d81a76087523931f3df507d502ec02be825b
정희호 <hujeong@hujeong.local>
2023-07-12 16:17:12 +0900

    feat: main page 레이아웃 작성

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 'use client'
-import { Container, Pagination, Stack, Grid, Divider, TextField, Box, InputAdornment, IconButton } from '@mui/material';
+import { Container, CssBaseline,Pagination, Stack, Grid, Divider, TextField, Box, InputAdornment, IconButton, List, ListItem, ListItemButton, ListItemText, FormControl, InputLabel, Select, MenuItem, Drawer, Button } from '@mui/material';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import QuestionListItem from '../components/QuestionListItem';
-import React, { Fragment } from 'react';
+import React, { useEffect, useState } from 'react';
 import './globals.css';
+import { Css, PaddingTwoTone } from '@mui/icons-material';
 
 
 type item = {
   index: number;
   title: string;
-}
+};
+
+type items = item[];
 
 function Header() {
   return (
@@ -27,24 +30,45 @@ function Header() {
   )
 }
 
+//function SearchBox() {
+//  return (
+//    <Box>
+//      <TextField
+//        id="outlined-search"
+//        type="search"
+//        placeholder="검색"
+//        size={'small'}
+//        sx={{paddingTop: '20px'}}
+//        InputProps={{
+//          endAdornment: (
+//            < InputAdornment position={'end'}>
+//              <IconButton
+//                onClick={() => {
+//                }}
+//              >
+//                <SearchOutlinedIcon />
+//              </IconButton>
+//            </InputAdornment>
+//          ),
+//        }}
+//      />
+//      <Button href='/write'>글쓰기</Button>
+//    </Box>
+//  )
+//}
+
 function SearchBox() {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'row-reverse',
-        bgcolor: 'background.paper',
-        borderRadius: 1,
-      }}
-    >
+    <Box sx={{ display: 'flex', alignItems: 'baseline' }}>
       <TextField
         id="outlined-search"
         type="search"
         placeholder="검색"
         size={'small'}
+        sx={{marginTop: '15px'}}
         InputProps={{
           endAdornment: (
-            < InputAdornment position={'end'}>
+            <InputAdornment position={'end'}>
               <IconButton
                 onClick={() => {
                 }}
@@ -55,11 +79,18 @@ function SearchBox() {
           ),
         }}
       />
+      <Button href='/write' sx={{ marginLeft: '5px' }}>글쓰기</Button>
     </Box>
   )
 }
 
-function QuestionPagination() {
+
+function QuestionPagination({page, setPage}: {page: number, setPage: React.Dispatch<React.SetStateAction<number>>}) {
+  
+  const handlePageChange= (event: React.ChangeEvent<unknown>, newPage: number) => {
+    setPage(newPage);
+  };
+  
   return (
     <Box
       sx={{
@@ -71,12 +102,71 @@ function QuestionPagination() {
         borderRadius: 1,
       }}
     >
-      <Pagination count={5} showFirstButton showLastButton />
+      <Pagination count={5} page={page} onChange={handlePageChange} showFirstButton showLastButton />
     </Box>
   );
 }
 
+function QuestionList({items}: {items: items}) {
+  return (
+    <List>
+      {items.map((item) => (
+        <QuestionListItem key={item.index} index={item.index} title={item.title}/>
+        ))}
+      <Divider />
+      {items.length < 10 && [...Array(10 - items.length)].map((_, index) => (
+        <Box key={index + 3}>
+          <ListItem sx={{ height: '48px'}}/>
+          <Divider />
+        </Box>
+      ))}
+    </List>
+  );
+}
+
+function SortSelectBox({sort, setSort}: {sort:string, setSort: React.Dispatch<React.SetStateAction<string>>}) {
+  return (
+      <FormControl>
+        <Select
+          value={sort}
+          onChange={(e) => {setSort(e.target.value as string)}}>
+            <MenuItem value={'latest'}>최신순</MenuItem>
+            <MenuItem value={'view'}>조회순</MenuItem>
+            <MenuItem value={'recommendation'}>추천순</MenuItem>
+        </Select>
+      </FormControl>
+  );
+}
+
+function SideBar() {
+  return (
+    <Drawer
+      sx={{
+        flexShrink: 1,
+        "&. MuiDrawer-paper": {
+          boxSizing: "border-box"
+        },
+        flexGrow: 0,
+      }}
+      variant='permanent'
+    >
+      <List>
+        {['all', 'minishell', 'ft_irc', 'minirt'].map((text) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text}/>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  )
+}
+
 export default function Home() {
+  const [page, setPage] = useState<number>(1);
+  const [sort, setSort] = useState<string>('latest');
+
   const items: item[] = [
     {
       index: 1,
@@ -89,23 +179,77 @@ export default function Home() {
     {
       index: 3,
       title: "minirt",
-    }
+    },
+    {
+      index: 4,
+      title: 'react',
+    },
+    //{
+    //  index: 5,
+    //  title: 'react',
+    //},
+    //{
+    //  index: 6,
+    //  title: 'react',
+    //},
+    //{
+    //  index: 7,
+    //  title: 'react',
+    //},
+    //{
+    //  index: 8,
+    //  title: 'react',
+    //},
+    //{
+    //  index: 9,
+    //  title: 'react',
+    //},
+    //{
+    //  index: 10,
+    //  title: 'react',
+    //},
+    //{
+    //  index: 11,
+    //  title: 'react',
+    //},
+    //{
+    //  index: 12,
+    //  title: 'react',
+    //},
+
   ];
 
   return (
-    <Container fixed>
-      <Header />
-      <Stack
-        spacing={2}
-        divider={<Divider orientation="horizontal" flexItem />}>
-        <SearchBox />
-        {items.map((item) => {
-          return (
-            <QuestionListItem key={item.index} index={item.index} title={item.title} />
-          );
-        })}
-        <QuestionPagination />
-      </Stack>
+    <Container sx={{
+                  display: 'flex',
+                  //flexDirection: 'column',
+                }}>
+      <CssBaseline />
+      <div
+        style={{
+          flexGrow: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          marginRight: '10px',
+          marginLeft: '80px'
+        }}
+      >
+        <Header />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            bgcolor: 'background.paper',
+            borderRadius: 1,
+          }}>
+          <SortSelectBox sort={sort} setSort={setSort}/>
+          <SearchBox />
+        </Box>
+        <QuestionList items={items}/>
+        <QuestionPagination setPage={setPage}page={page}/>
+      </div>
+      <SideBar/>
     </Container>
   );
 }
--- components/QuestionListItem.tsx
+++ components/QuestionListItem.tsx
@@ -1,17 +1,52 @@
 
 import React from 'react';
-import { Stack, Link } from "@mui/material";
+import { Box, Divider, List, ListItem, ListItemButton, ListItemText} from "@mui/material";
 
 function QuestionListItem({ index, title }: { index: number, title: string }): JSX.Element {
   return (
-    <Link href='/detail' underline="none" color="none">
-      <Stack spacing={3} direction="row" mt={3}>
-        <div>{index}</div>
-        <div>{title}</div>
-      </Stack>
-    </Link>
-
+    <Box>
+      <Divider/>
+    <ListItem key={index} disablePadding>
+      <ListItemButton href='/detail'>
+        <ListItemText primary={title}/>
+      </ListItemButton>
+    </ListItem>
+    </Box>
   );
 }
 
 export default QuestionListItem;
+
+//import * as React from 'react';
+//import Box from '@mui/material/Box';
+//import InputLabel from '@mui/material/InputLabel';
+//import MenuItem from '@mui/material/MenuItem';
+//import FormControl from '@mui/material/FormControl';
+//import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+//export default function BasicSelect() {
+//  const [age, setAge] = React.useState('');
+
+//  const handleChange = (event: SelectChangeEvent) => {
+//    setAge(event.target.value as string);
+//  };
+
+//  return (
+//    <Box sx={{ minWidth: 120 }}>
+//      <FormControl fullWidth>
+//        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+//        <Select
+//          labelId="demo-simple-select-label"
+//          id="demo-simple-select"
+//          value={age}
+//          label="Age"
+//          onChange={handleChange}
+//        >
+//          <MenuItem value={10}>Ten</MenuItem>
+//          <MenuItem value={20}>Twenty</MenuItem>
+//          <MenuItem value={30}>Thirty</MenuItem>
+//        </Select>
+//      </FormControl>
+//    </Box>
+//  );
+//}
