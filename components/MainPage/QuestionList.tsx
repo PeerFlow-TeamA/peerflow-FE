4659fd847a8c35cdab52e8f6aedf971466593a16
hujeong <hujeong@student.42seoul.kr>
2023-07-16 22:36:10 +0900

    feat: view, recommend icon 추가

--- components/MainPage/QuestionList.tsx
+++ components/MainPage/QuestionList.tsx
@@ -1,7 +1,8 @@
 import { Box, Divider, List, ListItem, ListItemButton, ListItemText} from "@mui/material";
 import {QuestionData, QuestionContent} from '../../interface/interface'
 import getCurrentTime from "@/util/util";
-
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 
 function QuestionListItem({ index, content, id}: { index: number, content: QuestionContent, id: number}): JSX.Element {
   const dateSplit = content.createdAt.split('/');
@@ -11,7 +12,19 @@ function QuestionListItem({ index, content, id}: { index: number, content: Quest
     <ListItem key={index} disablePadding>
       <ListItemButton href={`/detail/${id}`}>
         <ListItemText primary={index + '. ' + content.title} secondary={content.category}/>
-        <div>{getCurrentTime().split('/')[0] === dateSplit[0] ? dateSplit[1].substring(0, 5) : dateSplit[0].substring(5)}</div>
+        <Box>
+          <div style={{
+            marginLeft: '10px',
+            fontSize: '14px',
+            justifyContent: 'center',
+          }}>{getCurrentTime().split('/')[0] === dateSplit[0] ? dateSplit[1].substring(0, 5) : dateSplit[0].substring(5)}</div>
+          <Box sx={{display: 'flex', alignItems: 'center', mt: '5px'}}>
+          <VisibilityIcon sx={{fontSize: '15px', lineHeight: '1.2'}}/>
+          <span style={{fontSize: '12px'}}>{` ${content.view} `}</span>
+          <ThumbUpOffAltIcon sx={{fontSize: '15px', ml: '5px'}}/>
+          <span style={{fontSize: '12px'}}>{` ${content.view} `}</span>
+          </Box>
+        </Box>
       </ListItemButton>
     </ListItem>
     </Box>
